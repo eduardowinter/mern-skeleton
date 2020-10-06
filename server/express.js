19f9476da169874c6c60b30670a42ffccba41e13
eduardowinter <eduardowb@gmail.com>
2020-10-05 23:35:02 -0300

    Adding the frontend to the mern skeleton.

--- server/express.js
+++ server/express.js
@@ -5,12 +5,23 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template'
+import path from 'path'
 
 import userRoutes from './routes/user.routes'
 
 import authRoutes from './routes/auth.routes'
 
+// Only for development. Comment out when bundling for prodution
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
+
+// Only for development. Comment out when bundling for prodution
+devBundle.compile(app)
+
+
+
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
@@ -22,13 +33,16 @@ app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
 app.use('/', userRoutes)
+app.use('/', authRoutes)
 
 app.get('/', (req, res) => {
     res.status(200).send(Template())
 })
 
-app.use('/', authRoutes)
+
 
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
--- template.js
+++ template.js
@@ -4,9 +4,12 @@ export default () => {
         <head>
             <meta charset="UTF-8">
             <title>MERN Skeleton</title>
+            <link rel="stylesheet" href="https://fonts.gogleapis.com/css?family=Roboto:100,300,400">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         </head>
         <body>
-            <div id="root">Hello World</div>
+            <div id="root"></div>
+            <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
     </html>`
 }
